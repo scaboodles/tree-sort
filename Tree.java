760dd1711b1e677db9cb15051d2473cd8976b646
scaboodles <owenj.wolff2@gmail.com>
2022-12-29 19:45:28 -0800

    added tree class with recursive access to nodes

---
+++ Tree.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Tree<T> {
+    private Node<T> root;
+
+    public Tree(T rootData) {
+        root = new Node<T>();
+        root.data = rootData;
+        root.children = new ArrayList<Node<T>>();
+    }
+
+    public static class Node<T> {
+        private T data;
+        private Node<T> parent;
+        private ArrayList<Node<T>> children;
+    }
+
+    public void iterAll(){
+        recurseIter(this.root);
+    } 
+    private void recurseIter(Node<T> node){
+        for(Node<T> child:node.children){
+            recurseIter(child);
+        }
+    }
+}
